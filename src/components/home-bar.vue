9cff50b7f565e235bba2e47482bb61eb75d1286c
lighttigerxiv <lighttigerxiv@gmail.com>
2023-11-08 15:11:56 +0000

    update profile picture

--- src/components/home-bar.vue
+++ src/components/home-bar.vue
@@ -6,7 +6,7 @@ const router = useRouter();
 
 <template>
     <button class="flex items-center home-bar" @click="router.push('/')">
-        <img src="@images/logo.png" class="h-11 w-11">
+        <img src="@images/logo.png" class="h-8 w-8 rounded-full mr-2">
         <div class="text-lg">Home</div>
     </button>
 </template>
